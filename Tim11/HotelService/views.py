fae07548b8ea65188a25ed42f02b663f6d030c9b
Novak Jovanović <novakjovanovic97@gmail.com>
2019-03-26 18:40:17 +0100

    Add files via upload

--- Tim11/HotelService/views.py
+++ Tim11/HotelService/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Hotel
 # Create your views here.
+
+def hotels(request):
+    return render(request, 'hotels_home.html')
+
+def searched_hotels(request):
+    search = request.POST.get('hotels_search')
+    hotels_list = Hotel.objects.filter(name__contains=search)
+    return render(request, 'hotels_searched.html', {'hotels': hotels_list})
