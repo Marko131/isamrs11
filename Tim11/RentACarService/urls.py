a84286339abad3af4b1d2d8202761ba3036d9e24
strale97 <strahinja066@gmail.com>
2019-05-06 22:24:04 +0200

    Brze rezervacije vozila i rent-a-car izvestaji

--- Tim11/RentACarService/urls.py
+++ Tim11/RentACarService/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import rentacars, searched_rentacars, search_vehicles, rentacar_detail
+from .views import rentacars, searched_rentacars, search_vehicles, rentacar_detail, reserve, rentacar_service_reports
 urlpatterns = [
     path('', rentacars, name='rentacars'),
     path('search', searched_rentacars, name='search_rentacars'),
     path('search_vehicles', search_vehicles, name='search_vehicles'),
     path('rentacar/<int:rentacar_id>', rentacar_detail, name='rentacar_with_id'),
+    path('rentacar_reservation/<int:reservation_id>', reserve, name='reserve'),
+    path('get_rentacar_service_reports', rentacar_service_reports, name='rentacar_service_reports')
 ]
--- Tim11/RentACarService/views.py
+++ Tim11/RentACarService/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from .models import RentACar, Vehicle
-# Create your views here.
+from .models import RentACar, Vehicle, VehicleReservation
+from django.contrib.auth.decorators import login_required
+from datetime import datetime, timedelta
+from django.http import JsonResponse
 
 def rentacars(request):
     return render(request, 'rentacar_home.html')
@@ -18,4 +20,86 @@ def search_vehicles(request):
 
 def rentacar_detail(request, rentacar_id):
     rentacar = get_object_or_404(RentACar, pk=rentacar_id)
-    return render(request, 'rentacar_id.html', {'rentacar': rentacar})
+    quick_reservations = VehicleReservation.objects.filter(vehicle__rentacar=rentacar, user__isnull=True)
+    return render(request, 'rentacar_id.html', {'rentacar': rentacar, 'reservations':quick_reservations})
+
+@login_required
+def reserve(request, reservation_id):
+    reservation = get_object_or_404(VehicleReservation, pk=reservation_id)
+    reservation.user = request.user
+    reservation.save()
+    return render(request, 'rentacar_home.html')
+
+def rentacar_service_reports(request):
+    today = VehicleReservation.objects.filter(
+        reserved_from__lt=datetime.today()) & VehicleReservation.objects.filter(
+        reserved_from__gt=datetime.today() - timedelta(days=1))
+    yesterday = VehicleReservation.objects.filter(
+        reserved_from__lt=datetime.today() - timedelta(days=1)) & VehicleReservation.objects.filter(
+        reserved_from__gt=datetime.today() - timedelta(days=2))
+    twodaysago = VehicleReservation.objects.filter(
+        reserved_from__lt=datetime.today() - timedelta(days=2)) & VehicleReservation.objects.filter(
+        reserved_from__gt=datetime.today() - timedelta(days=3))
+    threedaysago = VehicleReservation.objects.filter(
+        reserved_from__lt=datetime.today() - timedelta(days=3)) & VehicleReservation.objects.filter(
+        reserved_from__gt=datetime.today() - timedelta(days=4))
+    fourdaysago = VehicleReservation.objects.filter(
+        reserved_from__lt=datetime.today() - timedelta(days=4)) & VehicleReservation.objects.filter(
+        reserved_from__gt=datetime.today() - timedelta(days=5))
+    fivedaysago = VehicleReservation.objects.filter(
+        reserved_from__lt=datetime.today() - timedelta(days=5)) & VehicleReservation.objects.filter(
+        reserved_from__gt=datetime.today() - timedelta(days=6))
+
+    oneweekago = VehicleReservation.objects.filter(
+        reserved_from__lt=datetime.today()) & VehicleReservation.objects.filter(
+        reserved_from__gt=datetime.today() - timedelta(weeks=1))
+    twoweeksago = VehicleReservation.objects.filter(
+        reserved_from__lt=datetime.today() - timedelta(weeks=1)) & VehicleReservation.objects.filter(
+        reserved_from__gt=datetime.today() - timedelta(weeks=2))
+    threeweeksago = VehicleReservation.objects.filter(
+        reserved_from__lt=datetime.today() - timedelta(weeks=2)) & VehicleReservation.objects.filter(
+        reserved_from__gt=datetime.today() - timedelta(weeks=3))
+    fourweeksago = VehicleReservation.objects.filter(
+        reserved_from__lt=datetime.today() - timedelta(weeks=3)) & VehicleReservation.objects.filter(
+        reserved_from__gt=datetime.today() - timedelta(weeks=4))
+
+    onemonthago = VehicleReservation.objects.filter(
+        reserved_from__lt=datetime.today()) & VehicleReservation.objects.filter(
+        reserved_from__gt=datetime.today() - timedelta(weeks=4))
+    twomonthsago = VehicleReservation.objects.filter(
+        reserved_from__lt=datetime.today() - timedelta(weeks=4)) & VehicleReservation.objects.filter(
+        reserved_from__gt=datetime.today() - timedelta(weeks=8))
+    threemonthsago = VehicleReservation.objects.filter(
+        reserved_from__lt=datetime.today() - timedelta(weeks=8)) & VehicleReservation.objects.filter(
+        reserved_from__gt=datetime.today() - timedelta(weeks=12))
+    fourmonthsago = VehicleReservation.objects.filter(
+        reserved_from__lt=datetime.today() - timedelta(weeks=12)) & VehicleReservation.objects.filter(
+        reserved_from__gt=datetime.today() - timedelta(weeks=16))
+
+    days = [
+        'Today',
+        'Yesterday',
+        '2 days ago',
+        '3 days ago',
+        '4 days ago',
+        '5 days ago',
+    ]
+    weeks = [
+        'One week ago',
+        'Two weeks ago',
+        'Three weeks ago',
+        'Four weeks ago',
+    ]
+    months = [
+        'One month ago',
+        'Two months ago',
+        'Three months ago',
+        'Four months ago'
+    ]
+    daysCount = [today.count(), yesterday.count(), twodaysago.count(), threedaysago.count(), fourdaysago.count(),
+                 fivedaysago.count()]
+    weeksCount = [oneweekago.count(), twoweeksago.count(), threeweeksago.count(), fourweeksago.count()]
+    monthsCount = [onemonthago.count(), twomonthsago.count(), threemonthsago.count(), fourmonthsago.count()]
+    return JsonResponse(
+        {'daysCount': daysCount, 'days': days, 'weeks': weeks, 'weeksCount': weeksCount, 'months': months,
+         'monthsCount': monthsCount})
